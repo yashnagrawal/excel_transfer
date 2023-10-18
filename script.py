5345e916dd7b0a407b0d571deb98638b17016cee
yashnagrawal <yashnagrawal@gmail.com>
2023-10-19 00:14:35 +0530

    Handled different names cases with lcs method

--- script.py
+++ script.py
@@ -15,42 +15,56 @@ sheet_a2 = workbook['A2']
 sheet_registered_list = workbook['Registered List']
 
 
+def longest_common_substring_length(s1, s2):
+    m = [[0] * (1 + len(s2)) for i in range(1 + len(s1))]
+
+    for x in range(1, 1 + len(s1)):
+        for y in range(1, 1 + len(s2)):
+            if s1[x - 1] == s2[y - 1]:
+                m[x][y] = m[x - 1][y - 1] + 1
+            else:
+                m[x][y] = max(m[x - 1][y], m[x][y - 1])
+
+    return m[len(s1)][len(s2)]
+
+
 def search_in_registered_list(name):
+    max_lcs_length = 0
+    ans_row = 0
+
     # find the student1 in the registered list name = Column B(first name) + Column C(last name)
     for registered_list_row in range(2, 99):
         registered_list_name = sheet_registered_list['A' + str(
-            registered_list_row)].value
-
-        if sheet_registered_list['B' + str(registered_list_row)].value != ".":
-            registered_list_name += " " + sheet_registered_list['B' + str(
+            registered_list_row)].value + " " + sheet_registered_list['B' + str(
                 registered_list_row)].value
 
         # if spaces is there after the name remove it
-        registered_list_name = registered_list_name.strip()
+        # registered_list_name = registered_list_name.strip()
 
-        # print(student1_name, registered_list_name)
-        if registered_list_name == name:
-            return registered_list_row
+        # find the longest common substring length
+        lcs_length = longest_common_substring_length(
+            name, registered_list_name)
 
-    return 0
+        # if the lcs length is greater than max_lcs_length
+        if lcs_length > max_lcs_length:
+            max_lcs_length = lcs_length
+            ans_row = registered_list_row
+
+    return ans_row
 
 
-def transfer_a1_to_registered_list():
+def transfer_to_registered_list(sheet, max_row):
     # iterate over rows 2 to 48 of Column B C Q
-    for row in range(2, 50):
+    for row in range(2, max_row):
         # get the student1 name
-        student1_name = sheet_a1['B' + str(row)].value
+        student1_name = sheet['B' + str(row)].value
         # get the student2 name
-        student2_name = sheet_a1['C' + str(row)].value
-
-        # if spaces is there after the name remove it
-        student1_name = student1_name.strip()
-        student2_name = student2_name.strip()
+        student2_name = sheet['C' + str(row)].value
 
         # get the marks of both in Q
-        marks = sheet_a1['Q' + str(row)].value
+        marks = sheet['Q' + str(row)].value
 
-        if student1_name != "-":
+        if student1_name != "-" and student1_name != "":
             # search for student1 in the registered list
             registered_list_row = search_in_registered_list(student1_name)
 
@@ -61,7 +75,7 @@ def transfer_a1_to_registered_list():
                 sheet_registered_list['G' + str(
                     registered_list_row)].value = marks
 
-        if student2_name != "-":
+        if student2_name != "-" and student2_name != "":
             # search for student2 in the registered list
             registered_list_row = search_in_registered_list(student2_name)
 
@@ -71,53 +85,12 @@ def transfer_a1_to_registered_list():
                 # set the marks of student2 in the registered list
                 sheet_registered_list['G' + str(
                     registered_list_row)].value = marks
-
-    # save the workbook
-    workbook.save('csf314-2023-Marks.xlsx')
-
-
-def transfer_a2_to_registered_list():
-    # iterate over rows 2 to 48 of Column B C Q
-    for row in range(2, 46):
-        # get the student1 name
-        student1_name = sheet_a2['B' + str(row)].value
-        # get the student2 name
-        student2_name = sheet_a2['C' + str(row)].value
-
-        # if spaces is there after the name remove it
-        student1_name = student1_name.strip()
-        student2_name = student2_name.strip()
-
-        # get the marks of both in S
-        marks = sheet_a2['S' + str(row)].value
-
-        if student1_name != "-":
-            # search for student1 in the registered list
-            registered_list_row = search_in_registered_list(student1_name)
-
-            if registered_list_row == 0:
-                print(student1_name)
-            else:
-                # set the marks of student1 in the registered list
-                sheet_registered_list['H' + str(
-                    registered_list_row)].value = marks
-
-        if student2_name != "-":
-            # search for student2 in the registered list
-            registered_list_row = search_in_registered_list(student2_name)
-
-            if registered_list_row == 0:
-                print(student2_name)
-            else:
-                # set the marks of student2 in the registered list
-                sheet_registered_list['H' + str(
-                    registered_list_row)].value = marks
-
-    # save the workbook
-    workbook.save('csf314-2023-Marks.xlsx')
 
 
 # call main function
 if __name__ == "__main__":
-    # transfer_a1_to_registered_list()
-    transfer_a2_to_registered_list()
+    transfer_to_registered_list(sheet_a1, 50)
+    transfer_to_registered_list(sheet_a2, 46)
+
+    # save the workbook
+    workbook.save('csf314-2023-Marks.xlsx')
